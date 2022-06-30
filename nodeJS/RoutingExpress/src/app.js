5b09dacc85f34fda637aa436692eac1bbefdbad7
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-06-30 14:54:39 -0300

    express - separação de responsabilidades

---
+++ nodeJS/RoutingExpress/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const tasks = require('../data/tasks.json')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const tasksRoutes = require("./routes/tasks")
+
+app.get|("/", (req, res) => {
+    res.status(200).send({"message": "API works"})
+})
+
+app.use("/tasks", tasksRoutes)
+
+module.exports = app
