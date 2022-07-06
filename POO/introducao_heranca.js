558e1295d499c6d0782fecb9339ae7fb8eb3488a
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-07-06 16:34:21 -0300

    Teste

--- POO/introducao_heranca.js
+++ POO/introducao_heranca.js
@@ -12,8 +12,29 @@ Animal.prototype.tipo = "desconhecido"
 function Cao(nome, tipo){
     this.nome = nome
     Animal.call(this, tipo)
-    this.constructor = Cao
+    // this.constructor = Cao
+    Object.defineProperty(Cao.prototype, "constructor", {
+        value: Cao,
+        enumerable: false
+    })
 }
 Cao.prototype = new Animal()
 
 let chica = new Cao("Chica", "mamifero")
+console.log(chica)
+console.log("-------------")
+
+for (let prop in chica){
+    if(chica.hasOwnProperty(prop)){
+        console.log(prop)
+    }
+}
+
+console.log("-------------")
+console.log(chica instanceof Cao)
+
+console.log("-------------")
+console.log(Object.prototype.isPrototypeOf(chica)) //Animal ou Object
+console.log(Object.getPrototypeOf(chica))
+console.log(chica.__proto__)
+console.log(chica.__proto__ === Object.getPrototypeOf(chica))
