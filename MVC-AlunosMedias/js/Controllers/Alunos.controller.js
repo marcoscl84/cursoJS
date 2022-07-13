18e6a6efbac8e778c73b6121545da6006072c11b
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-07-13 17:52:39 -0300

    Criação Model e Services

---
+++ MVC-AlunosMedias/js/Controllers/Alunos.controller.js
@@ -0,0 +1,3 @@
+class AlunoController {
+  constructor (service, view) {}
+}
--- MVC-AlunosMedias/js/app.js
+++ MVC-AlunosMedias/js/app.js
@@ -1,13 +1,38 @@
+const alunos = [
+  {
+    _id: 0,
+    nome: 'chico melato',
+    notas: {
+      portugues: [1, 1, 2, 2],
+      matematica: [2, 2, 2, 2],
+      historia: [2, 2, 3, 3],
+      ciencias: [3, 3, 3, 3]
+    }
+  },
+  {
+    _id: 1,
+    nome: 'talita lima',
+    notas: {
+      portugues: [4, 4, 4, 4],
+      matematica: [4, 4, 5, 5],
+      historia: [5, 5, 6, 6],
+      ciencias: [7, 7, 8, 9]
+    }
+  }
+]
+
+const alunosService = new AlunosService()
+
 // Calcula média de cada aluno por materia e cria uma propriedade chamada média
 alunos.forEach(aluno => {
-  aluno.media = {}
-
-  for (let materia in aluno.notas) {
-    aluno.media[materia] = avarege(...aluno.notas[materia])
-  }
+  alunosService.add(new AlunoModel(aluno))
 })
 
 console.log(alunos)
+
+/* 
+
+TODAS AS FUNÇÕES ABAIXO SERÃO DE RESPONSABILIDADE DA VIEW
 
 // Inserir no thead o nome e cada uma das matérias
 const htmlHeader = document.createElement('tr')
@@ -26,12 +51,43 @@ console.log(htmlHeader)
 document.querySelector('.data-table-alunos').appendChild(htmlHeader)
 
 // percorre cada aluno e gera html para incluir no TBODY
-alunos.forEach(aluno => {
-  const htmlBody = document.createElement('tr')
-  let htmlMedias = `<td>${aluno.nome}</td>`
-  Object.keys(aluno.notas).forEach(materia => {
-    htmlMedias += `<td>${aluno.media[materia]}</td>`
+function render () {
+  document.querySelector('.data-table-medias').innerHTML = ''
+  alunos.forEach(aluno => {
+    const htmlBody = document.createElement('tr')
+    let htmlMedias = `<td>${aluno.nome}</td>`
+    Object.keys(aluno.notas).forEach(materia => {
+      htmlMedias += `<td>${aluno.media[materia]}</td>`
+    })
+    htmlBody.innerHTML = htmlMedias
+    document.querySelector('.data-table-medias').appendChild(htmlBody)
   })
-  htmlBody.innerHTML = htmlMedias
-  document.querySelector('.data-table-medias').appendChild(htmlBody)
+}
+render()
+
+// Acionando alunos
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault() //para form não ser enviado
+  const nome = document.getElementById('first_name').value
+  const newAluno = {
+    _id: 0,
+    nome,
+    notas: {
+      portugues: [1, 1, 2, 2],
+      matematica: [2, 2, 2, 2],
+      historia: [2, 2, 3, 3],
+      ciencias: [3, 3, 3, 3]
+    }
+  }
+
+  newAluno.media = {}
+
+  for (let materia in newAluno.notas) {
+    newAluno.media[materia] = avarege(...newAluno.notas[materia])
+  }
+
+  alunos.push(newAluno)
+
+  render()
 })
+ */
